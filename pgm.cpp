6216fe17697e659fe005b2113c1c11ee3af4ce63
S.Magdy <pc@DESKTOP-RUU7O3T>
2022-08-31 05:45:04 +0200

    Using operator overloading to add the data of 2 pgm files

--- pgm.cpp
+++ pgm.cpp
@@ -1,15 +1,113 @@
 /***************************************************************************/
 #include "pgm.h"
 /***************************************************************************/
- pgm::pgm(int width, int height)
-{
-	this->Width = Width;
-};
+using namespace std;
 /***************************************************************************/
- void pgm::ReadPgm(std::stringstream ss)
+ pgm::pgm(std::string FileName)
+{
+	int rows, cols;
+	int rowIndex, colIndex, pixel;
+
+	stringstream ss;
+	string inputLine = "";
+	ifstream infile(FileName);
+
+	getline(infile, inputLine);      /* read the first line : P2 */
+	if (inputLine.compare("P2") != 0)
+		cerr << "Version error" << endl;
+	cout << "Version : " << inputLine << endl;
+
+	ss << infile.rdbuf();   /* read width and height */
+	ss >> cols >> rows;
+	cout << cols << " columns and " << rows << " rows" << endl;
+
+	int MaxVal;
+	ss >> MaxVal;
+	cout << "Max value is " << MaxVal << endl;
+	//ss.ignore();
+
+
+	/* updating the pgm properties */
+	this->Height = rows;
+	this->Width = cols;
+	this->MaxValue = MaxVal;
+	this->DataPtr = new int* [rows];
+	for (int i = 0; i < rows;i++)
+		DataPtr[i] = new int[cols];
+
+	/* updating the pgm pixels */
+	for (rowIndex = 0; rowIndex < (this->Height); rowIndex++)
+	{
+		for (colIndex = 0; colIndex < (this->Width); colIndex++)
+		{
+			ss >> pixel;
+			this->DataPtr[rowIndex][colIndex] = pixel;
+			std::cout << " " << pixel;
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+	infile.close();
+}
+/***************************************************************************/
+ pgm::pgm(int height = 0, int width = 0)
  {
-	 this->Width = Width;
+	 this->Height = height;
+	 this->Width = width;
+
+	 this->DataPtr = new int* [height];
+	 for (int i = 0; i < height;i++)
+	 {
+		 DataPtr[i] = new int[width];
+	 }
  };
+ /***************************************************************************/
+ void pgm::WritePgm(std::string FileName)
+ {
+	 int row, col;
+	 std::ofstream outfile(FileName);
+
+	 outfile << this->MagicNum << endl;
+	 outfile << this->Width << " " << this->Height << endl;
+	 outfile << this->MaxValue << endl;
+
+	 for (row = 0; row < (this->Height); row++)
+	 {
+		 for (col = 0; col < (this->Width); col++)
+		 {
+			 outfile << this->DataPtr[row][col] << " ";   // check if endl is needed
+		 }
+		 outfile << std::endl;
+	 }
+	 outfile.close();
+ };
+/***************************************************************************/
+ pgm pgm::operator+(pgm const& InputImage)
+ {
+	 int row, col;
+	 pgm result(this->Height, this->Width);
+
+	 for (int row = 0; row < this->Height; row++)
+	 {
+		 for (int col = 0; col < this->Width; col++)
+		 {
+			 result.DataPtr[row][col] = this->DataPtr[row][col] + InputImage.DataPtr[row][col];
+		 }
+		 result.SetMaxValue(this->MaxValue);
+		 result.SetMagicNum("P2");
+	 }
+	 return result;
+ }
+ /***************************************************************************/
+ pgm::~pgm()
+ {
+	 int row;
+	 if (DataPtr != NULL)
+	 {
+		 //for (row = 0; row < (this->Height); row++)
+			// delete DataPtr[row];
+	 }
+ }
 /***************************************************************************/
 void pgm::SetWidth(int Width)
 {
@@ -24,18 +122,17 @@ void pgm::SetHeight(int Height)
 void pgm::SetMaxValue(int MaxValue)
 {
 	this->MaxValue = MaxValue;
-};
+}
 /***************************************************************************/
-void pgm::SetMagicNum(int MagicNum)
+void pgm::SetMagicNum(std::string MagicNum)
 {
 	this->MagicNum = MagicNum;
 };
 /***************************************************************************/
 /***************************************************************************/
-/***************************************************************************/
 int pgm::GetWidth(void)
 {
-	return this->MagicNum;
+	return this->Width;
 };
 /***************************************************************************/
 int pgm::GetHeight(void)
@@ -48,8 +145,9 @@ int pgm::GetMaxValue(void)
 	return this->MaxValue;
 };
 /***************************************************************************/
-int pgm::GetMagicNum(void)
+std::string pgm::GetMagicNum(void)
 {
 	return this->MagicNum;
 };
+/***************************************************************************/
 /***************************************************************************/
--- pgm.h
+++ pgm.h
@@ -1,5 +1,6 @@
+#include <fstream>
 #include <sstream>
-#include <sstream>
+#include <iostream>  // to be removed
 
 #pragma once
 class pgm
@@ -8,19 +9,24 @@ class pgm
 		int Width;
 		int Height;
 		int MaxValue;
-		int MagicNum;
-		int* DataPtr;
+		std::string MagicNum;
+		int** DataPtr;
 	public:
-		pgm(int width, int height);
-		void ReadPgm(std::stringstream ss);
+		pgm(std::string FileName);
+		pgm(int height, int width);
+
+		void WritePgm(std::string FileName);
+
+		pgm operator + (pgm const& obj);
+		~pgm();
 
 		void SetWidth(int Width);
 		void SetHeight(int Height);
 		void SetMaxValue(int MaxValue);
-		void SetMagicNum(int MagicNum);
+		void SetMagicNum(std::string MagicNum);
 
 		int GetWidth(void);
 		int GetHeight(void);
 		int GetMaxValue(void);
-		int GetMagicNum(void);
+		std::string GetMagicNum(void);
 };
